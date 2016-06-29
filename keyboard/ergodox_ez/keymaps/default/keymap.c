5bfdbddfdcf29d742450554048d049aa1a7f5749
chiastolite <chiastolite.1980@gmail.com>
2016-06-29 14:29:41 +0900

    Add Reset Key

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                               KC_HOME,
                                                                KC_BSPC,KC_DELT,KC_END,
         // right hand
-             KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+             RESET,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              MO(FN),      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              TO(MOUSE,1), KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
